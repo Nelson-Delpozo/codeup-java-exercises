51c7d437c8721767a7cbe55f8e398dbb61c8bc90
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-15 10:44:36 -0600

    fixed newline

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -7,6 +7,6 @@
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
-        System.out.print("World!");
+        System.out.println("World!");
     }
 }
