940926fcfae30ec8fe34db189aae6e4896b20274
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-15 13:54:03 -0600

    exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -8,5 +8,45 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.print("Hello, ");
         System.out.println("World!");
+        int myFavoriteNumber = 26;
+        System.out.println(myFavoriteNumber);
+        String myString = "3.1429";
+        System.out.println(myString);
+//        long myNumber = 123;
+        float myNumber = 3;
+        System.out.println(myNumber);
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//        String class = "hi";
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+//        int three = (int) "three";
+
+        int x = 4;
+//        x = x + 5;
+        x += 5;
+        System.out.println(x);
+
+        int w = 3;
+        int y = 4;
+        y *= w;
+        System.out.println(y);
+
+        int z = 10;
+        int m = 2;
+        z /= m;
+        System.out.println(z);
+        z -= m;
+        System.out.println(z);
+
+        int maxNum = Integer.MAX_VALUE;
+        System.out.println(maxNum);
+
     }
 }
