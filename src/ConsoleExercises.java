b5609b8e279901d91f829b4e8a8ce97b310f9f77
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-16 09:51:16 -0600

    I/O exercises

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -13,6 +13,29 @@ public class ConsoleExercises {
 
         System.out.println("You entered: --> \"" + userInput + "\" <--");
 
+        System.out.print("Enter an integer: ");
+        int userInteger = scanner.nextInt();
+
+        System.out.println("You entered: --> \"" + userInteger + "\" <--");
+
+        System.out.print("Enter first word: ");
+        String userFirstWord = scanner.next();
+        System.out.print("Enter second word: ");
+        String userSecondWord = scanner.next();
+        System.out.print("Enter third word: ");
+        String userThirdWord = scanner.next();
+
+        System.out.println("You entered: --> \"" + userFirstWord + "\" <--");
+        System.out.println("You entered: --> \"" + userSecondWord + "\" <--");
+        System.out.println("You entered: --> \"" + userThirdWord + "\" <--");
+
+        Scanner scanner2 = new Scanner(System.in);
+        System.out.print("Enter a sentence: ");
+        String userSentence = scanner2.nextLine();
+        System.out.println("You said: " + userSentence);
+
+
+
 
 
 
