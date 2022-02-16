c7827d565b2869775177f5d4217a453e2e9ef6c3
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-16 09:32:43 -0600

    second exercise

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,20 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
 
     public static void main(String[] args){
         double pi = 3.14159;
-        System.out.printf("The value of pi is approximately %s", pi);
+        System.out.printf("The value of pi is approximately %s%n", pi);
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter something: ");
+        String userInput = scanner.nextLine();
+
+        System.out.println("You entered: --> \"" + userInput + "\" <--");
+
+
+
 
     }
 
