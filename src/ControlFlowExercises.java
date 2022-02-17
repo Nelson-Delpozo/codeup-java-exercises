7e7144b3c7fd19f9a5d3ef8a25dac6aef76461eb
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-17 09:46:14 -0600

    first exercise

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -2,11 +2,18 @@ public class ControlFlowExercises {
 
     public static void main(String[] args) {
 
-        int i = 0;
-        while (i < 10) {
-            System.out.println("i is " + i);
+//        int i = 0;
+//        while (i < 10) {
+//            System.out.println("i is " + i);
+//            i++;
+//        }
+
+        int i = 5;
+        while ( i <= 15){
+            System.out.println("i is: " + i);
             i++;
         }
+
 
     }
 }
