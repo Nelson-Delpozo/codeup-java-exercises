a81ee40d366df3e47e2589219580708720f4e294
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-17 10:27:47 -0600

    comments

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -8,11 +8,34 @@ public class ControlFlowExercises {
 //            i++;
 //        }
 
+//        Loop Basics
+//
+//        While
+//
+//        Create an integer variable i with a value of 5.
+//        Create a while loop that runs so long as i is less than or equal to 15
+//        Each loop iteration, output the current value of i, then increment i by one.
+//        Your output should look like this:
+//
+//        5 6 7 8 9 10 11 12 13 14 15
+
         int i = 5;
         while (i <= 15) {
             System.out.println("i is: " + i);
             i++;
         }
+
+//        Do While
+//
+//        Create a do-while loop that will count by 2's starting with 0 and ending at 100. Follow each number with a new line.
+//        Alter your loop to count backwards by 5's from 100 to -10.
+//        Create a do-while loop that starts at 2, and displays the number squared on each line while the number is less than 1,000,000. Output should equal:
+//
+//        2
+//        4
+//        16
+//        256
+//        65536
 
         int z = 2;
         do {
@@ -32,6 +55,9 @@ public class ControlFlowExercises {
             s *= s;
         } while (s <= 1000000);
 
+//        For
+//        Refactor the previous two exercises to use a for loop instead.
+
         for (int p = 5; p <= 15; p += 1) {
             System.out.println("p is " + p);
         }
@@ -48,7 +74,14 @@ public class ControlFlowExercises {
             System.out.println("w is " + w);
         }
 
-//        FIZZBUZZ
+//        Fizzbuzz
+//
+//        One of the most common interview questions for entry-level programmers is the FizzBuzz test. Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+//
+//                Write a program that prints the numbers from 1 to 100.
+//        For multiples of three: print “Fizz” instead of the number.
+//        For the multiples of five: print “Buzz”.
+//        For numbers which are multiples of both three and five: print “FizzBuzz”.
 
         for (int fizzBuzz = 1; fizzBuzz <= 100; fizzBuzz++) {
             if (fizzBuzz % 3 == 0 && fizzBuzz % 5 == 0) {
@@ -60,6 +93,29 @@ public class ControlFlowExercises {
                 System.out.println("Fizz");
             }else System.out.println(fizzBuzz);
         }
+
+//        Display a table of powers.
+//
+//                Prompt the user to enter an integer.
+//        Display a table of squares and cubes from 1 to the value entered.
+//        Ask if the user wants to continue.
+//        Assume that the user will enter valid data.
+//        Only continue if the user agrees to.
+//        Example Output
+//
+//
+//        What number would you like to go up to? 5
+//
+//        Here is your table!
+//
+//                number | squared | cubed
+//                ------ | ------- | -----
+//                1      | 1       | 1
+//        2      | 4       | 8
+//        3      | 9       | 27
+//        4      | 16      | 64
+//        5      | 25      | 125
+
 
 
 
