be09f77ae57232c0cde86326ee37ed8574b47377
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-18 11:02:19 -0600

    lesson things

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -1,7 +1,8 @@
 public class MethodsExercises {
 
     public static void main(String[] args) {
-        System.out.println(sayHello("Nelson"));
+//        System.out.println(sayHello("Nelson"));
+        System.out.println(getPower(3,5));
 
     }
 
@@ -11,6 +12,18 @@ public class MethodsExercises {
         return String.format("Hello, %s!", name);
     }
 
+
+
+    public static long getPower(int base, int exponent) {
+        if (exponent == 0) {
+            return 1;
+        } else if (exponent == 1) {
+            return base;
+        } else if (exponent == 2) {
+            return (long) base * base;
+        }
+        return base * getPower(base, exponent - 1);
+    }
 
 
 
