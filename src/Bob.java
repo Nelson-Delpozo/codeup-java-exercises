cb1b8ec6b6290a764dfda430e7fff87b67f840da
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-18 09:15:58 -0600

    finished first four

--- src/Bob.java
+++ src/Bob.java
@@ -1,6 +1,6 @@
 public class Bob {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
 //        String Basics.
 //
@@ -11,8 +11,21 @@ public class Bob {
         String exercise = "We don\'t need no education\nWe don\'t need no thought control";
         System.out.println(exercise);
 
+//        Check "this" out!, "s inside of "s!
 
+        String exercise2 = "Check \"this\" out!, \"s inside of \"s!";
+        System.out.println(exercise2);
 
+//        In windows, the main drive is usually C:\
+
+        String exercise3 = "In windows, the main drive is usually C:\\";
+        System.out.println(exercise3);
+
+//        I can do backslashes \, double backslashes \\,
+//        and the amazing triple backslash \\\!
+
+        String exercise4 = "I can do backslashes \\, double  backslashes \\\\,\nand the amazing triple backslash\\\\\\!";
+        System.out.println(exercise4);
 
 
     }
