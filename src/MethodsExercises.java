cb05e20624004246e0f45088bdbbdf9deb0376db
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-18 11:26:54 -0600

    methods bonuses

--- src/MethodsExercises.java
+++ src/MethodsExercises.java
@@ -2,7 +2,13 @@ public class MethodsExercises {
 
     public static void main(String[] args) {
 //        System.out.println(sayHello("Nelson"));
-        System.out.println(getPower(3,5));
+//        System.out.println(getPower(3,5));
+        System.out.println(addition(5,10));
+        System.out.println(subtraction(5,10));
+        System.out.println(multiplication(5,10));
+        System.out.println(division(5,10));
+        System.out.println(modulus(5,10));
+        System.out.println(multiplicationWithLoop(5,10));
 
     }
 
@@ -25,6 +31,32 @@ public class MethodsExercises {
         return base * getPower(base, exponent - 1);
     }
 
+    public static long addition(int a, int b){
+        return a + b;
+    }
+
+    public static long subtraction(long a, long b){
+        return a - b;
+    }
+
+    public static long multiplication(long a, long b){
+        return a * b;
+    }
+
+    public static long division(long a, long b){
+        return a / b;
+    }
+
+    public static long modulus(long a, long b){
+        return a % b;
+    }
+
+    public static long multiplicationWithLoop(int a, int b){
+        int multiple = a;
+        for (int i = a; i <= b; i++) {
+            multiple += i;
+        }return multiple;
+    }
 
 
 
