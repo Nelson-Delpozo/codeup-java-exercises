0086487a4e72329f1d3b75243927f0bee024eb15
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-18 09:11:14 -0600

    finished first one

--- src/Bob.java
+++ src/Bob.java
@@ -2,11 +2,19 @@ public class Bob {
 
     public static void main(String[] args){
 
+//        String Basics.
+//
+//        Create a class named StringExercise with a main method.
+//
+//        For each of the following output examples: create a String variable that contains the desired output and print it out to the console, you can do this with only one String variable and one print statement for each output example.
+
+        String exercise = "We don\'t need no education\nWe don\'t need no thought control";
+        System.out.println(exercise);
+
 
 
 
 
     }
-
 
 }
