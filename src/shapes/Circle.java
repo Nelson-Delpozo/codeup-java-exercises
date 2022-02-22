8671bfdf19810aaea3899f8033979414f15de675
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-22 16:00:28 -0600

    circle exercise

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,6 +1,20 @@
 package shapes;
 
 public class Circle {
+    private final double radius;
 
+    public Circle(double radius) {
+       this.radius =  radius;
+    }
+
+    public double getArea(){
+        return this.radius * this.radius * Math.PI;
+
+    }
+
+    public double getCircumference(){
+        return this.radius * (2 * Math.PI);
+
+    }
 
 }
