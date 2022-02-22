8a62b2126137eae22db0cfecdc581565358e0718
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-22 10:24:17 -0600

    constructor one

--- src/Person.java
+++ src/Person.java
@@ -1,6 +1,10 @@
 public class Person {
     private String name;
 
+    public Person(String name) {
+        this.name = name;
+    }
+
     public String getName(){
 //TODO: return the person's name
         return this.name;
@@ -16,8 +20,31 @@ public class Person {
     }
 
     public static void main(String[] args) {
-        Person person = new Person();
-        person.setName("Fred");
+        Person person = new Person("Fred");
+//        person.setName("Fred");
         person.sayHello();
+
+        Person person1 = new Person("Person1");
+//        person1.setName("Person1");
+        Person person2 = new Person("Person2");
+//        person2.setName("Person1");
+        System.out.println(person1.getName().equals(person2.getName()));
+        System.out.println(person1 == person2);
+
+        Person person3 = new Person("Jojo");
+//        person3.setName("Jojo");
+        Person person4 = person3;
+        System.out.println(person3 == person4);
+
+        Person person5 = new Person("Guy");
+//        person5.setName("Guy");
+        Person person6 = person5;
+        System.out.println(person5.getName());
+        System.out.println(person6.getName());
+        person6.setName("Jane");
+        System.out.println(person5.getName());
+        System.out.println(person6.getName());
+
     }
 }
+
