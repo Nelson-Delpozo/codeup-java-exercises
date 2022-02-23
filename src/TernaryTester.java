de218e8894a85a49130bb23cfc2015ab3e55457f
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-23 09:24:53 -0600

    java 1 quiz

---
+++ src/TernaryTester.java
@@ -0,0 +1,15 @@
+class ArithmeticSample {
+    public static void main(String[] args) {
+
+        String y = "hello";
+        int x = 6;
+
+
+
+        System.out.println(y + x);
+    }}
+
+
+
+
+
