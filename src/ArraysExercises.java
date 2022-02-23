034448aad17471cbc64d8dee837cb433392e8dad
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-23 12:30:53 -0600

    arrays exercises

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -4,9 +4,34 @@ public class ArraysExercises {
 
     public static void main(String[] args) {
 
-        int[] numbers = {1, 2, 3, 4, 5};
-        System.out.println(Arrays.toString(numbers));
+//        int[] numbers = {1, 2, 3, 4, 5};
+//        System.out.println(Arrays.toString(numbers));
+
+        Person[] people = {new Person("Mr. Pink"), new Person("Mr. White"), new Person("Nice Guy Eddie")};
+
+        for (int i = 0; i < people.length; i++) {
+            System.out.println("people[" + i + "] = " + people[i].getName());
+        }
+
+    }
+
+    public static Person[] addPerson(Person[] people, Person newPerson) {
+        newPerson = new Person("New Guy");
+        Person[] newPeople = new Person[people.length + 1];
+
     }
 
 
+
+
 }
+
+
+
+
+
+
+
+
+
+
