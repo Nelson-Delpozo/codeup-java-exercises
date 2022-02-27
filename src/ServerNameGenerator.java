51afaff331b2a86258f38996b939a5e8624bedca
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-27 09:42:27 -0600

    starting server name generator

---
+++ src/ServerNameGenerator.java
@@ -0,0 +1,23 @@
+//Server Name Generator
+//
+//        We are going to build a server name generator. Create a class inside of src named ServerNameGenerator, and follow the specs below.
+//
+//        Create two arrays whose elements are strings: one with at least 10 adjectives, another with at least 10 nouns.
+//        Create a method that will return a random element from an array of strings.
+//        Add a main method, and inside of your main method select and random noun and adjective and hyphenate the combination and display the generated name to the user.
+//        Example Output
+//
+//
+//        Here is your server name:
+//        dedicated-photon
+
+
+
+
+
+public class ServerNameGenerator {
+
+
+
+
+}
