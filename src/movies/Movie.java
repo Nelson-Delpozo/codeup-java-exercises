4b3003a9a5a1e435e8557e85c120fdd3c25f3215
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-28 10:44:51 -0600

    movies exercises

---
+++ src/movies/Movie.java
@@ -0,0 +1,27 @@
+package movies;
+
+public class Movie {
+    private String title;
+    private String category;
+
+    public Movie(String title, String category) {
+        this.title = title;
+        this.category = category;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+}
