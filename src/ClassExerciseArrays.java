098430553debb3ef109459cdbd9e7d8dc62b784c
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-28 15:27:58 -0600

    array class stuff

--- src/ClassExerciseArrays.java
+++ src/ClassExerciseArrays.java
@@ -8,13 +8,23 @@ public class ClassExerciseArrays {
         weatherTypes[2] = "cloudy";
 
         System.out.println(weatherTypes[1]);
-
+       sumAll();
     }
 
 
+    public static void sumAll(){
+        Integer[] nums = new Integer[5];
+        int sum = 0;
+        nums[0] = 5;
+        nums[1] = 10;
+        nums[2] = 15;
+        nums[3] = 11;
+        nums[4] = 8;
 
-
-
-
+        for (Integer num : nums) {
+            sum += num;
+        }
+        System.out.println(sum);
+    }
 
 }
