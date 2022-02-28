8c38bcabcffae831c86fa421646c9fcad3441f20
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-28 11:55:36 -0600

    finished movies for now

--- src/movies/MoviesApplication.java
+++ src/movies/MoviesApplication.java
@@ -2,35 +2,79 @@ package movies;
 
 import util.Input;
 
-import java.util.Arrays;
-
 public class MoviesApplication {
 
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         Input inputChoice = new Input();
 
-        System.out.println("What would you like to do?");
-        System.out.println("0 - exit");
-        System.out.println("1 - view all movies");
-        System.out.println("2 - view movies in the animated category");
-        System.out.println("3 - view movies in the drama category");
-        System.out.println("4 - view movies in the horror category");
-        System.out.println("5 - view movies in the sci-fi category");
-        System.out.print("Enter your choice: ");
 
-        int choice = inputChoice.getInt();
-        System.out.println("choice = " + choice);
+do {
+    System.out.println("What would you like to do?");
+    System.out.println("0 - exit");
+    System.out.println("1 - view all movies");
+    System.out.println("2 - view movies in the animated category");
+    System.out.println("3 - view movies in the drama category");
+    System.out.println("4 - view movies in the horror category");
+    System.out.println("5 - view movies in the sci-fi category");
+    System.out.print("Enter your choice: ");
 
-        Movie[] movieArray = MoviesArray.findAll();
-        System.out.println(Arrays.toString(movieArray));
+    int choice = inputChoice.getInt();
+    System.out.println("choice = " + choice);
 
-        System.out.println(movieArray[0].getTitle() + " -- " + movieArray[0].getCategory());
+    Movie[] movieArray = MoviesArray.findAll();
+
+//        System.out.println(Arrays.toString(movieArray));
+//        System.out.println(movieArray[0].getTitle() + " -- " + movieArray[0].getCategory());
+
+    if (choice == 1) {
+
+        for (Movie movie : movieArray) {
+            System.out.println(movie.getTitle() + " -- " + movie.getCategory());
+            System.out.println();
+        }
+
+    } else if (choice == 2) {
+
+        for (Movie movie : movieArray) {
+            if (movie.getCategory().equals("animated")) {
+                System.out.println(movie.getTitle() + " -- " + movie.getCategory());
+                System.out.println();
+            }
+        }
+
+    } else if (choice == 3) {
+        for (Movie movie : movieArray) {
+            if (movie.getCategory().equals("drama")) {
+                System.out.println(movie.getTitle() + " -- " + movie.getCategory());
+                System.out.println();
+            }
+        }
+
+    } else if (choice == 4) {
+
+        for (Movie movie : movieArray) {
+            if (movie.getCategory().equals("horror")) {
+                System.out.println(movie.getTitle() + " -- " + movie.getCategory());
+                System.out.println();
+            }
+        }
+
+    } else if (choice == 5) {
+
+        for (Movie movie : movieArray) {
+            if (movie.getCategory().equals("sci-fi")) {
+                System.out.println(movie.getTitle() + " -- " + movie.getCategory());
+                System.out.println();
+            }
+        }
+    }else if(choice == 0){
+        return;
+    }
+
+}while(true);
 
 
     }
-
-
-
 }
