85c6302eb51dcf279063b9228acbdb37dbdb6cdb
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-02-28 14:11:27 -0600

    class exercises

---
+++ src/ClassExerciseArrays.java
@@ -0,0 +1,20 @@
+public class ClassExerciseArrays {
+
+    public static void main(String[] args) {
+        String[] weatherTypes = new String[3];
+
+        weatherTypes[0] = "rainy";
+        weatherTypes[1] = "sunny";
+        weatherTypes[2] = "cloudy";
+
+        System.out.println(weatherTypes[1]);
+
+    }
+
+
+
+
+
+
+
+}
