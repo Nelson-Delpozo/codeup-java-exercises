0b1b52746f76db055b548bbe015de5430304a653
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-01 10:48:54 -0600

    add a person

--- src/ArraysExercises.java
+++ src/ArraysExercises.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ArraysExercises {
 
     public static void main(String[] args) {
@@ -11,11 +13,25 @@ public class ArraysExercises {
             System.out.println("people[" + i + "] = " + people[i].getName());
         }
 
+        Person[] people1 = Arrays.copyOf(people, people.length + 1);
+        System.out.println("people1 = " + Arrays.toString(people1));
+        people1[3] = new Person("Mr. Yellow");
+        System.out.println("people1 = " + Arrays.toString(people1));
+
+        for (int i = 0; i < people1.length; i++) {
+            System.out.println("people1[" + i + "] = " + people1[i].getName());
+        }
+
+
     }
+    //        END OF MAIN
 
 
 
 
+
+
+//END OF CLASS
 }
 
 
