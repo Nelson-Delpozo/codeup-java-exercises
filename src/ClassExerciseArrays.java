83862a066c0f7c9153b37df2602cb2480403353e
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-01 09:16:07 -0600

    morning exercise

--- src/ClassExerciseArrays.java
+++ src/ClassExerciseArrays.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ClassExerciseArrays {
 
     public static void main(String[] args) {
@@ -9,6 +11,7 @@ public class ClassExerciseArrays {
 
         System.out.println(weatherTypes[1]);
         System.out.println("sumAll() = " + sumAll());
+        System.out.println(Arrays.toString(sumOddAndEven()));
     }
 
 
@@ -25,6 +28,34 @@ public class ClassExerciseArrays {
             sum += num;
         }
         return sum;
+    }
+
+
+    public static Integer[] sumOddAndEven(){
+        Integer[] numbers = new Integer[10];
+        int sumOfEvens = 0;
+        int sumOfOdds = 0;
+        Integer[] finalArray = new Integer[2];
+        numbers[0] = 1;
+        numbers[1] = 2;
+        numbers[2] = 3;
+        numbers[3] = 4;
+        numbers[4] = 5;
+        numbers[5] = 6;
+        numbers[6] = 7;
+        numbers[7] = 8;
+        numbers[8] = 9;
+        numbers[9] = 10;
+
+        for (Integer num : numbers) {
+            if(num % 2 == 0){
+                sumOfEvens += num;
+            }else
+            sumOfOdds += num;
+        }
+        finalArray[0] = sumOfOdds;
+        finalArray[1] = sumOfEvens;
+        return finalArray;
     }
 
 
