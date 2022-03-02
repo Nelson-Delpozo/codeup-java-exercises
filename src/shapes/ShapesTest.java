427e6009afd26ee5d9f4238f3eef674419416ff3
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-02 11:29:37 -0600

    finished abstract class exercises

--- src/shapes/ShapesTest.java
+++ src/shapes/ShapesTest.java
@@ -9,6 +9,11 @@ public class ShapesTest {
         System.out.println("myShape.getArea() = " + myShape.getArea());
         System.out.println("myShape.getPerimeter() = " + myShape.getPerimeter());
 
+        Measurable myOtherShape = new Rectangle(10, 15);
+        System.out.println("myOtherShape.getArea() = " + myOtherShape.getArea());
+        System.out.println("myOtherShape.getPerimeter() = " + myOtherShape.getPerimeter());
+
+
 
 //        Rectangle box1 = new Rectangle(4, 5);
 //        System.out.println("box1.getArea() = " + box1.getArea());
