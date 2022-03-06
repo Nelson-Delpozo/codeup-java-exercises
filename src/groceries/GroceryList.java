f4bd3a839d1106009d04408838b5a6fab6f5c408
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-06 15:52:28 -0600

    constructor in list

--- src/groceries/GroceryList.java
+++ src/groceries/GroceryList.java
@@ -1,5 +1,20 @@
 package groceries;
 
+import java.util.ArrayList;
+
 public class GroceryList {
 
+    ArrayList<GroceryItem> groceries;
+
+    public GroceryList(ArrayList<GroceryItem> groceries) {
+        this.groceries = groceries;
+    }
+
+    public ArrayList<GroceryItem> getGroceries() {
+        return groceries;
+    }
+
+    public void setGroceries(ArrayList<GroceryItem> groceries) {
+        this.groceries = groceries;
+    }
 }
