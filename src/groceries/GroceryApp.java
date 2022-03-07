2ca28ce3d9c26e0c30217e66b3e30837b72e2336
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-07 11:16:49 -0600

    updated list

--- src/groceries/GroceryApp.java
+++ src/groceries/GroceryApp.java
@@ -17,52 +17,52 @@ public class GroceryApp {
         String startNewList = groceryScanner.nextLine();
 
 
-        if(startNewList.equals("y")){
+        if (startNewList.equals("y")) {
             System.out.print("Would you like to add an item?[y/N]: ");
             String addNewItem = groceryScanner.nextLine();
 
-            if (addNewItem.equals("y")){
+            if (addNewItem.equals("y")) {
                 boolean done = false;
-                do{
-                System.out.print("What item?: ");
-                String newItem = groceryScanner.next();
-                System.out.print("What's the category?: ");
-                String newItemCategory = groceryScanner.next();
-                System.out.print("How many?: ");
-                int newItemQuantity = groceryScanner.nextInt();
+                do {
+                    System.out.print("What item?: ");
+                    String newItem = groceryScanner.next();
+                    System.out.print("What's the category?: ");
+                    String newItemCategory = groceryScanner.next();
+                    System.out.print("How many?: ");
+                    int newItemQuantity = groceryScanner.nextInt();
 
-                System.out.printf("Would you like to add this item: %s --> category: %s --> quantity: %s [y/N]? ", newItem, newItemCategory, newItemQuantity);
-                Scanner itemAdder = new Scanner(System.in);
-                String addItem = itemAdder.nextLine();
+                    System.out.printf("Would you like to add this item: %s --> category: %s --> quantity: %s [y/N]? ", newItem, newItemCategory, newItemQuantity);
+                    Scanner itemAdder = new Scanner(System.in);
+                    String addItem = itemAdder.nextLine();
 
-                if(addItem.equals("y")){
-                    GroceryItem newItemToAdd = new GroceryItem(newItem, newItemCategory, newItemQuantity);
-                    groceries.add(newItemToAdd);
-                }
+                    if (addItem.equals("y")) {
+                        GroceryItem newItemToAdd = new GroceryItem(newItem, newItemCategory, newItemQuantity);
+                        groceries.add(newItemToAdd);
+                    }
 
-                System.out.print("Would you like to add another item[y/N]? ");
-                String addAnother = itemAdder.nextLine();
-                if(addAnother.equalsIgnoreCase("n")){
-                    done = true;
-                }
-                }while(!done);
+                    System.out.print("Would you like to add another item[y/N]? ");
+                    String addAnother = itemAdder.nextLine();
+                    if (addAnother.equalsIgnoreCase("n")) {
+                        done = true;
+                    }
+                } while (!done);
 
-            }else System.exit(0);
+            } else System.exit(0);
 
-        }else System.exit(0);
-
-        GroceryItem tofu = new GroceryItem("tofu", "proteins", 4);
-        GroceryItem beans = new GroceryItem("beans", "proteins", 2);
-        GroceryItem broccoli = new GroceryItem("broccoli", "veggies", 2);
-        GroceryItem soymilk = new GroceryItem("soymilk", "drinks", 1);
+        } else System.exit(0);
 
 
-        groceries.add(tofu);
-        groceries.add(beans);
-        groceries.add(broccoli);
-        groceries.add(soymilk);
+        groceries.add(new GroceryItem("quinoa", "grains", 1));
+        groceries.add(new GroceryItem("veggie chips", "snacks", 2));
+        groceries.add(new GroceryItem("salsa", "snacks", 1));
+        groceries.add(new GroceryItem("spaghetti", "pasta", 1));
+        groceries.add(new GroceryItem("red peppers", "veggies", 2));
+        groceries.add(new GroceryItem("tofu", "vegan", 3));
+        groceries.add(new GroceryItem("chick peas", "beans", 2));
+        groceries.add(new GroceryItem("broccoli", "veggies", 2));
+        groceries.add(new GroceryItem("soymilk", "drinks", 1));
 
-        soymilk.setQuantity(10);
+
         boolean chooseSort = false;
         do {
             System.out.print("How would you like to see your list? [1=alpha 2=category]: ");
@@ -74,14 +74,15 @@ public class GroceryApp {
                 groceries.sort(Comparator.comparing(GroceryItem::getCategory));
                 chooseSort = true;
             }
-        }while(!chooseSort);
+        } while (!chooseSort);
 
         for (GroceryItem grocery : groceries) {
             System.out.println(grocery.getItem() + " -- " + grocery.getCategory() + " -- " + grocery.getQuantity());
         }
 
-    }
-
-
 
     }
+//    END OF MAIN METHOD
+
+}
+//END OF CLASS
