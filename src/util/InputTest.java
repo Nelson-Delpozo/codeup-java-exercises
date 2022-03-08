eface45b4c905213317a8b8773a6e9ba5cd6742b
Nelson Delpozo <nelson.delpozo99@gmail.com>
2022-03-08 09:04:56 -0600

    error handling

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -9,6 +9,7 @@ public class InputTest {
         System.out.println("input.getString() = " + input.getString());
         System.out.println("input.yesNo() = " + input.yesNo());
         System.out.println("input.getInt(2, 50) = " + input.getInt(2, 50));
+        System.out.print("Enter an Integer: ");
         System.out.println(input.getInt());
         System.out.println("input.getDouble(2.5, 50.5) = " + input.getDouble(2.5, 50.5));
         System.out.println(input.getDouble());
